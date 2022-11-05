c7fc3e2e296ddd69d6fd61300c63c1ad4db8c305
trungnq <tjny6789@gmail.com>
2022-11-05 10:25:24 +0700

    make migration

--- database/migrations/2022_11_02_005858_user_role.php
+++ database/migrations/2022_11_02_005858_user_role.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRolesTable extends Migration
+class UserRole extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('user_role', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign("user_id")->references('id')->on('users');
+            $table->foreign("role_id")->references('id')->on('roles');
         });
     }
 
@@ -27,6 +29,6 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('user_role');
     }
 }
